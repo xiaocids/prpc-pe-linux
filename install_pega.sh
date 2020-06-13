75aacc4866734888ee6ce1b8c1e03a52be5833c0
R Awaludin <xiaocids@live.com>
2020-06-13 21:26:00 +0700

    Update install_pega.sh
    update certificates for poetgtes 9.6

--- install_pega.sh
+++ install_pega.sh
@@ -5,7 +5,7 @@ PEGA_TEMP="/tmp/pegatemp"
 #Add sources to install pljava
 sudo apt-get install -y curl ca-certificates
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 #update system
 sudo apt update && sudo apt -y upgrade
